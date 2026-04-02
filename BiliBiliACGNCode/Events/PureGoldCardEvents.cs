750f669d65df6eff2b76a1a1c34a0d2b3003c980
imwheat <62542063+imwheat@users.noreply.github.com>
2026-04-02 16:19:07 +0800

    update 更新场景背景更新

--- BiliBiliACGNCode/Events/PureGoldCardEvents.cs
+++ BiliBiliACGNCode/Events/PureGoldCardEvents.cs
@@ -7,10 +7,15 @@
 using BiliBiliACGN.BiliBiliACGNCode.Cards;
 using BiliBiliACGN.BiliBiliACGNCode.Core.Models.Encounters;
 using BiliBiliACGN.BiliBiliACGNCode.Relics;
+using Godot;
+using MegaCrit.Sts2.Core.Assets;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Context;
 using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
 using MegaCrit.Sts2.Core.Rewards;
 using MegaCrit.Sts2.Core.Runs;
 
@@ -21,6 +26,8 @@ public sealed class PureGoldCardEvents : EventBaseModel
     public override bool IsShared => true;
     public override IReadOnlySet<Type> OwnerActTypes => new HashSet<Type> { };
     public override EventLayoutType LayoutType => EventLayoutType.Default;
+    private static readonly string _trialNondescriptVfx = SceneHelper.GetScenePath("vfx/events/trial_nondescript_vfx");
+    private static string ParkPath => ImageHelper.GetImagePath("events/pure_gold_card_park.png");
     public override EncounterModel? CanonicalEncounter => ModelDb.Encounter<PureGoldCardEncounter>();
     protected override IReadOnlyList<EventOption> GenerateInitialOptions()
     {
@@ -43,6 +50,19 @@ public sealed class PureGoldCardEvents : EventBaseModel
         await RelicCmd.Obtain<PureGoldCard>(base.Owner);
     }
     /// <summary>
+    /// 添加vfx到事件场景图片
+    /// </summary>
+    /// <param name="portraitPath"></param>
+    private void AddVfxAnchoredToPortrait(string portraitPath)
+	{
+		if (LocalContext.IsMe(base.Owner))
+		{
+			Node2D node2D = PreloadManager.Cache.GetScene(portraitPath).Instantiate<Node2D>(PackedScene.GenEditState.Disabled);
+			node2D.Position = new Vector2(292f, 68f);
+			NEventRoom.Instance.Layout.AddVfxAnchoredToPortrait(node2D);
+		}
+	}
+    /// <summary>
     /// 物归原主
     /// 决定去附近的龟有公园派驻所物归原主。
     /// </summary>
@@ -50,6 +70,13 @@ public sealed class PureGoldCardEvents : EventBaseModel
     /// <exception cref="NotImplementedException"></exception>
     private async Task GiveItBack()
     {
+        AddVfxAnchoredToPortrait(_trialNondescriptVfx);
+        // 设置事件场景图片
+        if (LocalContext.IsMe(base.Owner))
+		{
+			NEventRoom.Instance.SetPortrait(PreloadManager.Cache.GetTexture2D(ParkPath));
+		}
+
         SetEventState(L10NLookup("PURE_GOLD_CARD.pages.PARK.description"), [
 			new EventOption(this, AcceptDuel, "PURE_GOLD_CARD.pages.PARK.options.DUEL", HoverTipFactory.FromCard<FourthBlueEyesWhiteDragon>()),
 			new EventOption(this, RefuseDuel, "PURE_GOLD_CARD.pages.PARK.options.REFUSE")
